d66d6e85049323b4f82e9c18e6928878bfb0485f
Vinod Tik <vinodtikhatri@gmail.com>
2018-03-19 23:35:22 +0530

    initial upload
    initial upload for the first time. basic setup js and html file.

---
+++ script.js
@@ -0,0 +1,79 @@
+var pizzaVeg = [
+	{
+		"name":"Deluxe Veggie",
+		"description":"Onion, Capsicum, Grilled Mashroom, Golden Corn, Paneer",
+		"prince": "299",
+		"category": "Veg",
+		"size": "M",
+		"image":"",
+	},
+	{
+		"name":"Veg Extravaganza",
+		"description":"Black Olives, Onion, Capsicum,Grilled Mushroom, Tomato, Golden Corn, Jalapeno, Extra Cheese",
+		"prince": "349",
+		"category": "Veg",
+		"size": "M",
+		"image":"",
+	},
+	{
+		"name":"5 Pepper",
+		"description":"Capsicum, Yellow, Red Bell Pepper, Jalapeno, Red Paprika",
+		"prince": "299",
+		"category": "Veg",
+		"size": "M",
+		"image":"",
+	},
+	{
+		"name":"Double Cheese Margherita",
+		"description":"Loaded with extra cheese!",
+		"prince": "305",
+		"category": "Veg",
+		"size": "M",
+		"image":"",
+	},
+	{
+		"name":"Cheese and Corn",
+		"description":"Cheese, Golden Corn",
+		"prince": "399",
+		"category": "Veg",
+		"size": "M",
+		"image":"",
+	},
+	{
+		"name":"Mexican Green Wave",
+		"description":"Onion, Crisp Capsicum, Fresh Tomato, Jalapeno sprinkled with exotic Mexican Herbs",
+		"prince": "399",
+		"category": "Veg",
+		"size": "M",
+		"image":"",
+	},
+	{
+		"name":"Cheese and Corn",
+		"description":"Cheese, Golden Corn",
+		"prince": "399",
+		"category": "Veg",
+		"size": "M",
+		"image":"",
+	},
+]
+
+var pizzaChicken = [
+	{
+		"name":"CHICKEN GOLDEN DELIGHT",
+		"description":"Double Pepper Barbecue Chicken, Goldern Corn, Extra Cheese",
+		"prince": "499",
+		"category": "Non Veg",
+		"size": "M",
+		"image":"",
+	},
+	{
+		"name":"Chicken Fiesta",
+		"description":"Grilled chicken Rashers, Peri-peri chicken, Onion, Capsicum",
+		"prince": "399",
+		"category": "Non Veg",
+		"size": "M",
+		"image":"chunky-chicken.png",
+	},
+]
+
+document.write("");
